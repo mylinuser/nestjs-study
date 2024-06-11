2279cb1ae8b2ef721a42a5b897442b7f2909bc28
linhaozhang <1158389909@qq.com>
2024-06-12 01:02:15 +0800

    feat: add blog management

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // 1. 固定路径：
+  // 可以匹配到 get请求，http://localhost:9080/app/list
+  @Get('list')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // 可以匹配到 post请求，http://localhost:9080/app/list
+  @Post('list')
+  create(): string {
+    return this.appService.getHello();
+  }
+
+  // 2.通配符路径(?+* 三种通配符 )
+  // 可以匹配到 get请求, http://localhost:9080/app/user_xxx
+  @Get('user_*')
+  getUser() {
+    return 'getUser';
+  }
+
+  @Put('list/user')
+  updateUser() {
+    return { userId: 1 };
+  }
+
+  // 3.带参数路径
+  // 可以匹配到put请求，http://localhost:9080/app/list/xxxx
+  @Put('list/:id')
+  update() {
+    return 'update';
   }
 }
